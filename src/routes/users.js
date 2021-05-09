f054857219f07406ee08953f23f7c5bb32b5a111
Rafael-Amorim7 <rafaamorim1504@gmail.com>
2021-05-09 17:02:39 -0300

    Finalizacao rotas

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { v4 as id } from uuid
 
-users = [];
+var users = [];
 
 const router =  express.Router();
 
@@ -15,6 +15,38 @@ router.post("/", (req,) => {
   users.push({ ...user, id: id()})
 
   res.send(`O usuario ${user.name} foi adicionado!`)
+})
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params
+
+  const userFind = users.find((user) => user.id === id)
+
+  res.send(userFind)
+})
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params
+
+  users = users.filter((user) => user.id !== id);
+})
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params
+  const { name, adress, cep, email } = req.body
+
+  const user = users.find((user) => user.id === id)
+
+  if (name)
+    user.name = name
+  if (adress)
+    user.adress = adress
+  if (cep)
+    user.cep = cep
+  if (email)
+    user.email = email
+
+  res.send(`User com id ${id} foi atualizado`)
 })
 
 export default router;
